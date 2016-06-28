5b864eabb120a55215256dc0e65b33da4b259e6a
Paul <paulalexrees@gmail.com>
2016-06-28 18:16:46 +0100

    fixed rspec errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,6 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'rails/all'
-require 'rspec/rails'
-require 'capybara/rspec'
-require 'capybara/rails'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
